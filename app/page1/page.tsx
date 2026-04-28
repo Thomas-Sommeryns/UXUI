c80d2bba723fab1b0a5ab87f065f5c82ea87e0e0
Thomas-Sommeryns <thomassommeryns06@gmail.com>
2026-04-28 20:47:59 +0200

    Add files via upload

---
+++ app/page1/page.tsx
@@ -0,0 +1,16 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h1 className="text-3xl font-bold">About Us</h1>
+      <p className="text-gray-700">
+        testtest
+      
+      </p>
+      <ul className="list-disc pl-5 text-gray-700 space-y-2">
+        <li>Innovative technology</li>
+        <li>Professional team</li>
+        <li>Customer satisfaction</li>
+      </ul>
+    </div>
+  )
+}
